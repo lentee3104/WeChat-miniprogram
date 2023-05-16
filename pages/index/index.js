c04042919dd4a28bb891c05319847727a0699b68
lentee <yuxinyang2003@126.com>
2023-05-16 21:55:59 +0800

    zanwu

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,9 +7,9 @@ Page({
     wx.redirectTo({
       url: '/pages/test01/test01',
     })
-  },
+  }
 
-  data: {
+  ,data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
@@ -43,57 +43,12 @@ Page({
       }
     })
   },
-
-  uploadImage: function(){
-    wx.chooseMedia({
-      count: 1,
-      mediaType: ['image'],
-      sourceType: ['album', 'camera'],
-      success: function (res){
-        //判断用户是否选择了文件
-        if(res.tempFiles && res.tempFiles.length > 0){
-          // 选择成功后，获取图片本地路径
-          var imagePath = res.tempFiles[0].tempFilePath;
-          // 根据图片路径创建一个canvas对象
-          var ctx = res.createSelectorQuery('myCanvas');
-          // 绘制上传的图片到canvas中
-          ctx.drawImage(imagePath, 0, 0, 300, 300);
-          // 对图片进行处理，比如添加滤镜
-          ctx.filter = 'grayscale(100%)';
-          // 将处理后的图片导出为本地临时文件
-          ctx.draw(false, function() {
-            wx.canvasToTempFilePath({
-              canvasId: 'myCanvas',
-              success: function (res) {
-                // 导出成功后，展示处理后的图片
-                wx.saveImageToPhotosAlbum({
-                  filePath: res.tempFilePath,
-                  success: function() {
-                    // 导出成功后，res.tempFilePath 即为临时文件的路径
-                    console.log(res.tempFilePath);
-                    wx.showToast({
-                      title: '保存成功',
-                      icon: 'success'
-                    })
-                  },
-                  fail: function() {
-                    wx.showToast({
-                      title: '保存失败',
-                      icon: 'none'
-                    })
-                  }
-                })
-              }
-            });
-          });
-        }else{
-          // 用户取消了选择文件的操作，不进行任何处理
-          console.log('用户取消了选择文件的操作');
-        }
-      },
-      fail: function(res) {
-        console.log(res);
-      }
+  getUserInfo(e) {
+    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+    console.log(e)
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
     })
   }
 })
